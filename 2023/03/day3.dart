7b2d2633c8702aa1c48df432df74cd00854307a3
Cameron Line <cmline@sep.com>
2023-12-09 12:46:41 -0500

    Part 2 finally >:(

--- 2023/03/day3.dart
+++ 2023/03/day3.dart
@@ -1,10 +1,11 @@
+import 'dart:collection';
 import 'dart:ffi';
 
 import '../Utils/dartUtils.dart';
 
 void main(){
   bool runP1 = true;
-  // bool runP2 = false;
+  bool runP2 = true;
   int solutionP1, solutionP2;
 
   var demoInput = padMatrix(parseInput(Utils.readToString("./demo-input.txt")), 10);
@@ -12,14 +13,14 @@ void main(){
   var input = padMatrix(parseInput((Utils.readToString("./input.txt"))), 140);
   var timeParse = stopwatch.elapsedMilliseconds;
 
-  if(runP1) solutionP1 = solvePart1(input);
+  // if(runP1) solutionP1 = solvePart1(input);
   var timeP1 = stopwatch.elapsedMilliseconds;
-  // if(runP2) solutionP2 = solvePart2(input);
+  if(runP2) solutionP2 = solvePart2(input);
   var timeP2 = stopwatch.elapsedMilliseconds;
 
   print('Parse time: ${timeParse * 1/1000}s');
-  if(runP1) print('Part 1 (${(timeP1 - timeParse) * 1/1000}s): ${solutionP1}');
-  // if(runP2) print('Part 2 (${(timeP2 - timeP1) * 1/1000}s): ${solutionP2}');
+  // if(runP1) print('Part 1 (${(timeP1 - timeParse) * 1/1000}s): ${solutionP1}');
+  if(runP2) print('Part 2 (${(timeP2 - timeP1) * 1/1000}s): ${solutionP2}');
   print('Ran in ${stopwatch.elapsedMilliseconds * 1/1000} seconds');
 }
 
@@ -43,17 +44,15 @@ List<List<String>> padMatrix(List<List<String>> input, int size) {
   return input;
 }
 
-bool checkSurroundings(int row, int col, List<List<String>> matrix) {
+bool checkSurroundings(int row, int col, List<List<String>> matrix, RegExp symbolMatch) {
   // check m[row][col-1], m[row][col+1] and m[row-1][col-1], m[row-1][col], m[row-1][col+1] and m[row+1][col-1], m[row+1][col], m[row+1][col+1] for a character that is not '.' or [1-9]
-  String checkAbove = row != 0 ? matrix[row-1][col-1] + matrix[row-1][col] + matrix[row-1][col+1]: '';
-  String checkBelow = row != 140 ? matrix[row+1][col-1] + matrix[row+1][col] + matrix[row+1][col+1] : '';
-  String checkLeft = col != 0 ? matrix[row][col-1] : '';
-  String checkRight = col != 140 ? matrix[row][col+1] : '';
-  RegExp symbolMatch = new RegExp(r'[^.|0-9]');
+  String checkAbove = matrix[row-1][col-1] + matrix[row-1][col] + matrix[row-1][col+1];
+  String checkBelow = matrix[row+1][col-1] + matrix[row+1][col] + matrix[row+1][col+1];
+  String checkLeft = matrix[row][col-1];
+  String checkRight = matrix[row][col+1];
   if (symbolMatch.hasMatch(checkLeft + checkRight 
                           + checkAbove
-                          + checkBelow
-                          )) return true;
+                          + checkBelow)) return true;
 
   return false;
 }
@@ -68,7 +67,7 @@ int solvePart1(List<List<String>> input){
     for (int col = 1; col < input.length; col++) {
       if (isDigit.hasMatch(input[row][col])) {
         number += input[row][col];
-        if (checkSurroundings(row, col, input)) adjacentToSymbol = true;
+        if (checkSurroundings(row, col, input, new RegExp(r'[^.|0-9]'))) adjacentToSymbol = true;
       } 
       else {
         if (adjacentToSymbol) total += int.parse(number);
@@ -80,6 +79,55 @@ int solvePart1(List<List<String>> input){
   return total;
 }
 
-int solvePart2(Object input) {
-  return 0;
+Map<String, List<int>> starsPerNumberMap = new HashMap();
+
+List<String> nextToStar(int row, int col, List<List<String>> matrix) {
+  List<String> stars = [];
+  RegExp matchStar = new RegExp(r'[*]');
+
+  if (matchStar.hasMatch(matrix[row-1][col-1])) stars.add('${row-1}/${col-1}');
+  if (matchStar.hasMatch(matrix[row-1][col])) stars.add('${row-1}/${col}');
+  if (matchStar.hasMatch(matrix[row-1][col+1])) stars.add('${row-1}/${col+1}');
+
+  if (matchStar.hasMatch(matrix[row+1][col-1])) stars.add('${row+1}/${col-1}');
+  if (matchStar.hasMatch(matrix[row+1][col])) stars.add('${row+1}/${col}');
+  if (matchStar.hasMatch(matrix[row+1][col+1])) stars.add('${row+1}/${col+1}');
+
+  if (matchStar.hasMatch(matrix[row][col-1])) stars.add('${row}/${col-1}');
+  if (matchStar.hasMatch(matrix[row][col+1])) stars.add('${row}/${col+1}');
+
+  return stars;
+}
+
+int solvePart2(List<List<String>> input) {
+  RegExp isDigit = new RegExp(r'[0-9]');
+  int total = 0;
+
+  for (int row = 1; row < input.length; row++) {
+    String number = '';
+    Set<String> stars = new Set();
+    for (int col = 1; col < input.length; col++) {
+      if (isDigit.hasMatch(input[row][col])) {
+        number += input[row][col];
+        nextToStar(row, col, input).forEach((c) { stars.add(c); });
+      } 
+      else {
+        for (String c in stars) {
+          // starsPerNumberMap[c]?.add(number as int) ??= [number as int];
+          if (number != '') {
+            if (starsPerNumberMap[c] != null) starsPerNumberMap[c]?.add(int.parse(number));
+            else starsPerNumberMap[c] = [int.parse(number)];
+          }
+        }
+        number = '';
+        stars = new Set();
+      }
+    }
+  }
+  starsPerNumberMap.forEach((key, value) {
+    if (value.length == 2) {
+      total += (value[0] * value[1]);
+    }
+  });
+  return total;
 }
